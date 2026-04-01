b8e48f31996f95f48f9e3be470b58c7817684de7
lordpietre <pietrelinux@gmail.com>
2026-04-01 00:43:05 +0000

    revision

--- migrations/reset_feeds.sql
+++
@@ -1,4 +0,0 @@
--- Migration: Reset all feeds to inactive with 0 attempts
--- Run this to set all feeds as inactive (activo = false) and reset fallbacks to 0
-
-UPDATE feeds SET activo = false, fallos = 0;
